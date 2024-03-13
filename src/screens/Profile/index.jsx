5f87df57c47319d6a989002a245a58059325288d
LucasRiosF <123502386+LucasRiosF@users.noreply.github.com>
2024-03-13 16:58:17 -0300

    Initial commit

---
+++ src/screens/Profile/index.jsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native'
+import React from 'react'
+
+import styles from "./styles"
+import Title from '../../components/Title';
+
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+      <Title title="Profile" />
+    </View>
+  );
+}
